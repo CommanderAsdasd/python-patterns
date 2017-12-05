c0592068c6123cd9bdbd2b7749921de89252ca29
Alexander Tankovskiy <atankovskiy@Alexanders-MacBook-Air.local>
2017-12-05 10:42:23 +0300

    Borg added

--- own_rewrite/Borg.py
+++ own_rewrite/Borg.py
@@ -4,13 +4,25 @@
 class Borg(object):
     __shared_state = {}
 
-    def __init__(self)
+    def __init__(self):
+        self.__dict__= self.__shared_state
+        self.state = 'Init'
 
-    def __str__(self)
-
+    def __str__(self):
+        return self.state
 
 class YourBorg():
+    pass
 
 if __name__ == '__main__':
     rm1 = Borg()
     rm2 = Borg()
+
+    print('rm1: {0}'.format(rm1))
+    print('rm2: {0}'.format(rm2))
+    print(rm1.__dict__)
+
+    rm1.state = 'lol_state'
+    
+    print('rm1: {0}'.format(rm1))
+    print('rm2: {0}'.format(rm2))
